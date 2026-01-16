498f8dbfded440c02b2b76130939be79ded76971
marvel17a <marvelstudio17a@gmail.com>
2026-01-16 23:20:43 +0530

    Update morning.js

--- static/js/morning.js
+++ static/js/morning.js
@@ -151,7 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div class="small text-danger fw-bold mt-1 stock-warning" style="display:none;"></div>
             </td>
             <td><input type="number" name="opening[]" class="table-input opening" value="${open}" readonly tabindex="-1"></td>
-            <td><input type="number" name="given[]" class="table-input given input-qty" min="0"   value="0"></td>
+            <td><input type="number" name="given[]" class="table-input given input-qty" placeholder="0"></td>
             <td><input type="number" name="total[]" class="table-input total" value="${open}" readonly tabindex="-1"></td>
             <td><input type="number" name="price[]" class="table-input price text-end" value="${price.toFixed(2)}" readonly tabindex="-1"></td>
             <td><input type="number" name="amount[]" class="table-input amount text-end fw-bold text-primary" value="0.00" readonly tabindex="-1"></td>
@@ -272,5 +272,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
 
 
